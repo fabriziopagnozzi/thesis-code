53ce0ace2983ab527c5d250730f1b1f5ba4b2b4b
fabriziopagnozzi <fabrizio.pagnozzi@mail.polimi.it>
2026-04-09 21:12:54 +0200

    simplify note chunking

--- src/helpers/chunks_classes.py
+++ src/helpers/chunks_classes.py
@@ -27,6 +27,5 @@ class MimicIVChunk(Chunk):
     hadm_id: int | None = None
     section_name: str | None = None
     subsection_name: str | None = None
-    bhc_category: str | None = None
     char_count: int = 0
     approx_tokens: int = 0
